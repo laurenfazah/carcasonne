0f19e9d43479ad6dace66b148e620145dde171b0
laurenfazah <laurenfazah@gmail.com>
2017-11-06 14:04:45 -0700

    Removed styleMap

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import styleMap from '../data/styleMap'
 
 class Tile extends Component {
   constructor(props) {
--- src/data/styleMap.js
+++
@@ -1,6 +0,0 @@
-const styleMap = {
-  "0,0": "absolute-middle",
-  "0,1": "above-middle"
-}
-
-export default styleMap
