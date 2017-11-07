c81e1038686c713e8e98bdff66d830a6586231ef
laurenfazah <laurenfazah@gmail.com>
2017-11-06 17:16:56 -0700

    Remove swp files and debugger

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -4,7 +4,6 @@ import styleMap from '../data/styleMap'
 class Tile extends Component {
   constructor(props) {
     super(props)
-    debugger;
     this.styles = {
    	  top: props.meta.domPosition.offsetTop,
    	  left: props.meta.domPosition.offsetLeft
